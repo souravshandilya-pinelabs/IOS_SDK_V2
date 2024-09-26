73b64f1d4d80d1a64b389ce059043c4febd1a291
Sourav Shandilya <sourav.shandilya@PINEMAC0421.local>
2024-09-26 12:00:32 +0530

    latest ios framework change

---
+++ IOS_SDK_V2.xcframework/ios-arm64/IOS_SDK_V2.framework/Headers/IOS_SDK_V2.h
@@ -0,0 +1,18 @@
+//
+//  IOS_SDK_V2.h
+//  IOS_SDK_V2
+//
+//  Created by Naman Pandey on 23/09/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IOS_SDK_V2.
+FOUNDATION_EXPORT double IOS_SDK_V2VersionNumber;
+
+//! Project version string for IOS_SDK_V2.
+FOUNDATION_EXPORT const unsigned char IOS_SDK_V2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IOS_SDK_V2/PublicHeader.h>
+
+
